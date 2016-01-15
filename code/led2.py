31cf7d9e65980c7f43f061c64ca504a7e7171738
leiquan <leiquan@baidu.com>
2016-01-16 03:09:21 +0800

    up

--- code/led2.py
+++ code/led2.py
@@ -5,7 +5,7 @@ import RPi.GPIO
 import time
 
 # 可以正常运行的代码
-# 指定GPIO口的选定模式为GPIO引脚编号模式（而非主板编号模式）
+# 指定GPIO口的选定模式为GPIO引脚编号模式（而非主板编号模式）,注意两者的区别
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 
 # 指定GPIO14（就是LED长针连接的GPIO针脚）的模式为输出模式
@@ -14,14 +14,14 @@ RPi.GPIO.setup(14, RPi.GPIO.OUT)
 
 # 循环10次
 for i in range(0, 10):
-	# 让GPIO14输出高电平（LED灯亮）
-	RPi.GPIO.output(14, True)
-	# 持续一段时间
-	time.sleep(0.5)
-	# 让GPIO14输出低电平（LED灯灭）
-	RPi.GPIO.output(14, False)
-	# 持续一段时间
-	time.sleep(0.5)
+    # 让GPIO14输出高电平（LED灯亮）
+    RPi.GPIO.output(14, True)
+    # 持续一段时间
+    time.sleep(0.5)
+    # 让GPIO14输出低电平（LED灯灭）
+    RPi.GPIO.output(14, False)
+    # 持续一段时间
+    time.sleep(0.5)
 
 # 最后清理GPIO口（不做也可以，建议每次程序结束时清理一下，好习惯）
 RPi.GPIO.cleanup()

6280d2722477525ea723535317b15599f700bc7c
leiquan <leiquan@baidu.com>
2016-01-16 01:27:37 +0800

    up

---
+++ led.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+while True:
+    GPIO.output(11, True)
+
+    time.sleep(1)
+    GPIO.output(11, False)
+    time.sleep(1)

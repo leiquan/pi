1142c54f61aaa7d2bea348d8a7cdb4b83ae54508
leiquan <leiquan@baidu.com>
2016-01-16 12:26:07 +0800

    up

--- code/car2.py
+++ code/car2.py
@@ -8,23 +8,28 @@ RPi.GPIO.setmode(RPi.GPIO.BCM)
 
 # 两个电机的开和关
 
-#电机A,对应14口
+# 电机A,对应14口
 RPi.GPIO.setup(14, RPi.GPIO.OUT)
 
-#电机A,对应的in1和in2
+# 电机A,对应的in1和in2
 RPi.GPIO.setup(6, RPi.GPIO.OUT)
 RPi.GPIO.setup(13, RPi.GPIO.OUT)
 
-#电机B,对应15口
+# 电机B,对应15口
 RPi.GPIO.setup(15, RPi.GPIO.OUT)
 
-#电机B,对应的in1和in2
+# 电机B,对应的in1和in2
 RPi.GPIO.setup(19, RPi.GPIO.OUT)
 RPi.GPIO.setup(26, RPi.GPIO.OUT)
 
+try:
+    # 第一个点击正转
+    RPi.GPIO.output(14, True)
+    RPi.GPIO.output(6, False)
+    RPi.GPIO.output(13, True)
 
-#第一个点击正转
-RPi.GPIO.output(14, True)
-RPi.GPIO.output(6, False)
-RPi.GPIO.output(13, True)
+except KeyboardInterrupt:
+    pass
 
+# 清理GPIO口
+RPi.GPIO.cleanup()

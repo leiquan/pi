2e99cb30cde778f5980d93a0a694ac55435a29b4
leiquan <leiquan@baidu.com>
2016-01-16 11:55:18 +0800

    加入了四个控制信号

--- code/car.py
+++ code/car.py
@@ -19,26 +19,26 @@ pwm.start(0)
 pwm2.start(0)
 
 try:
-	while True:
-		# 电流从小到大，LED由暗到亮
-		for i in xrange(0, 101, 1):
-			# 更改占空比，
-			pwm.ChangeDutyCycle(i)
-			pwm2.ChangeDutyCycle(i)
-			time.sleep(.02)
+    while True:
+        # 电流从小到大，LED由暗到亮
+        for i in xrange(0, 101, 1):
+            # 更改占空比，
+            pwm.ChangeDutyCycle(i)
+            pwm2.ChangeDutyCycle(i)
+            time.sleep(.02)
 
-		# 再让电流从大到小，LED由亮变暗
-		for i in xrange(100, -1, -1):
-			pwm.ChangeDutyCycle(i)
-			pwm2.ChangeDutyCycle(i)
-			time.sleep(.02)
+        # 再让电流从大到小，LED由亮变暗
+        for i in xrange(100, -1, -1):
+            pwm.ChangeDutyCycle(i)
+            pwm2.ChangeDutyCycle(i)
+            time.sleep(.02)
 
 # 最后一段是一个小技巧。这个程序如果不强制停止会不停地执行下去。
 # 而Ctrl+C强制终端程序的话，GPIO口又没有机会清理。
 # 加上一个try except 可以捕捉到Ctrl+C强制中断的动作，
 # 试图强制中断时，程序不会马上停止而是会先跳到这里来做一些你想做完的事情，比如清理GPIO口。
 except KeyboardInterrupt:
-	pass
+    pass
 
 # 停用 PWM
 pwm.stop()
@@ -46,3 +46,21 @@ pwm2.stop()
 
 # 清理GPIO口
 RPi.GPIO.cleanup()
+
+# 前面的 pwm 已经正确生效
+RPi.GPIO.setup(6, RPi.GPIO.OUT)
+RPi.GPIO.setup(13, RPi.GPIO.OUT)
+RPi.GPIO.setup(19, RPi.GPIO.OUT)
+RPi.GPIO.setup(26, RPi.GPIO.OUT)
+while True:
+    RPi.GPIO.output(6, True)
+    RPi.GPIO.output(13, True)
+    RPi.GPIO.output(19, True)
+    RPi.GPIO.output(26, True)
+
+    time.sleep(1)
+    RPi.GPIO.output(6, False)
+    RPi.GPIO.output(13, False)
+    RPi.GPIO.output(19, False)
+    RPi.GPIO.output(26, False)
+    time.sleep(1)

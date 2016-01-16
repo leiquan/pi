2eb110a76c456293201e0c4f38ef531720bffb22
leiquan <leiquan@baidu.com>
2016-01-16 16:12:48 +0800

    up

---
+++ code/testclean.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import RPi.GPIO
+import time
+
+RPi.GPIO.setmode(RPi.GPIO.BCM)
+
+RPi.GPIO.cleanup()
---
+++ code/testoff.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import RPi.GPIO
+import time
+
+
+RPi.GPIO.setmode(RPi.GPIO.BCM)
+
+n=14
+RPi.GPIO.setup(n, RPi.GPIO.OUT)
+RPi.GPIO.output(n, False)

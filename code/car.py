9e183d5d581e7737c4176ceb1204661985fb6d89
leiquan <leiquan@baidu.com>
2016-01-16 11:58:10 +0800

    把 pwm 放在了1234之后

--- code/car.py
+++ code/car.py
@@ -8,6 +8,26 @@ RPi.GPIO.setmode(RPi.GPIO.BCM)
 RPi.GPIO.setup(14, RPi.GPIO.OUT)
 RPi.GPIO.setup(15, RPi.GPIO.OUT)
 
+
+# 前面的 pwm 已经正确生效,这里尝试01赋值
+RPi.GPIO.setup(6, RPi.GPIO.OUT)
+RPi.GPIO.setup(13, RPi.GPIO.OUT)
+RPi.GPIO.setup(19, RPi.GPIO.OUT)
+RPi.GPIO.setup(26, RPi.GPIO.OUT)
+while True:
+    RPi.GPIO.output(6, True)
+    RPi.GPIO.output(13, True)
+    RPi.GPIO.output(19, True)
+    RPi.GPIO.output(26, True)
+
+    time.sleep(2)
+    RPi.GPIO.output(6, False)
+    RPi.GPIO.output(13, False)
+    RPi.GPIO.output(19, False)
+    RPi.GPIO.output(26, False)
+    time.sleep(2)
+
+
 # 呼吸灯的效果
 # 创建一个 PWM 实例，需要两个参数，第一个是GPIO端口号，这里我们用14号
 # 第二个是频率（Hz），频率越高LED看上去越不会闪烁，相应对CPU要求就越高，设置合适的值就可以
@@ -46,21 +66,3 @@ pwm2.stop()
 
 # 清理GPIO口
 RPi.GPIO.cleanup()
-
-# 前面的 pwm 已经正确生效
-RPi.GPIO.setup(6, RPi.GPIO.OUT)
-RPi.GPIO.setup(13, RPi.GPIO.OUT)
-RPi.GPIO.setup(19, RPi.GPIO.OUT)
-RPi.GPIO.setup(26, RPi.GPIO.OUT)
-while True:
-    RPi.GPIO.output(6, True)
-    RPi.GPIO.output(13, True)
-    RPi.GPIO.output(19, True)
-    RPi.GPIO.output(26, True)
-
-    time.sleep(1)
-    RPi.GPIO.output(6, False)
-    RPi.GPIO.output(13, False)
-    RPi.GPIO.output(19, False)
-    RPi.GPIO.output(26, False)
-    time.sleep(1)
